038797de790162bbca97ae8891826fcfea3ec66c
Mark <mso872@gmail.com>
2020-02-16 21:26:47 -0800

    Added longitude, humidity, wind speed, and city name display functionality

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -7,13 +7,28 @@ $( document ).ready(function() {
     $("#searchbutton").on("click", function(){
        let city =  $("#searchinput").val();
 
-       $.ajax({url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}`, success: function(result){
+       $.ajax({url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${api}`, success: function(result){
         console.log(result)
+        
+        let name = result.name;
+        console.log(`This is the city name: ${name}`)
+        $("#name").text(name)
+
         let lon = result.coord.lon;
         console.log(`This is the long: ${lon}`)
-        let temp = result.main.temp;
-        console.log(`this is the temp: ${temp}`)
+        $("#lon").text(lon)
+
+        let temp = result.main.temp + "°F";
+        console.log(`this is the temperature: ${temp}`)
         $("#temp").text(temp)
+
+        let humidity = result.main.humidity;
+        console.log(`this is the humidity: ${humidity}`)
+        $("#humidity").text(humidity)
+
+        let speed = result.wind.speed;
+        console.log(`this is the wind speed: ${speed}`)
+        $("#windspeed").text(speed)
        
       }})
     })
